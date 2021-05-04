919cab0fef169b6d00732cedabbb74d042fc6793
Kartik Mahato <kartikmahato1996@gmail.com>
2021-05-04 14:14:21 +0530

    project added githubUser

--- src/pages/Timeline/Timeline.js
+++ src/pages/Timeline/Timeline.js
@@ -6,11 +6,13 @@ import Card from 'react-bootstrap/Card';
 
 // projects
 import instagramClone from '../../assets/img/projects/insta-clone.gif';
+import githubUsers from '../../assets/img/projects/githubuser.png';
 
 // skills
 import L_REACT from '../../assets/img/skills/react.svg';
 import L_FIREBASE from '../../assets/img/skills/firebase.svg';
 import L_GITHUB from '../../assets/img/skills/github.svg';
+import L_AUTH0 from '../../assets/img/skills/auth0.svg';
 
 import './Timeline.css';
 
@@ -20,6 +22,93 @@ const ProjectTimeline = () => {
             <h1 className="pt-3 text-center font-details-b pb-3">PROJECTS</h1>
             <Timeline>
                 <Events>
+                    <ImageEvent
+                        date="02/02/2021"
+                        className="text-center"
+                        text="Github Users"
+                        src={githubUsers}
+                        alt="Github Users"
+                    >
+                        <div className="d-flex justify-content-between flex-column mt-1">
+                            <div>
+                                <Accordion>
+                                    <Card>
+                                        <Accordion.Toggle
+                                            as={Card.Header}
+                                            eventKey="0"
+                                            className="p-2 text-center accordian-main"
+                                        >
+                                            PROJECT DETAILS
+                                        </Accordion.Toggle>
+
+                                        <Accordion.Collapse eventKey="0" className="text-left">
+                                            <Card.Body>
+                                                <strong>Description:</strong>A React based web application to search for various github users.
+                                                <hr />
+                                                <strong>Features:</strong>
+                                                <ul className="list-styles pt-1">
+                                                    <li>User can register/login</li>
+                                                    <li>User can look for different users along with their repos, their most starred/forked repos.</li>
+                                                </ul>
+                                                <hr />
+                                                <strong>Tech used:</strong>
+                                                <ul>
+                                                    <li>
+                                                        <span className="p-2">
+                                                            <Image
+                                                                src={L_REACT}
+                                                                alt="React.js"
+                                                                rounded
+                                                                className="image-style m-1"
+                                                            ></Image>{" "}
+                                                            REACT.JS
+                                                        </span>
+                                                    </li>
+                                                    <li>
+                                                        <span className="p-2">
+                                                            <Image
+                                                                src={L_AUTH0}
+                                                                alt="CSS 3"
+                                                                rounded
+                                                                className="image-style m-1"
+                                                            ></Image>{" "}
+                                                            AUTH0
+                                                        </span>
+                                                    </li>
+                                                    <li>
+                                                        <span className="p-2">
+                                                            <Image
+                                                                src={L_GITHUB}
+                                                                alt="Material-UI"
+                                                                rounded
+                                                                className="image-style1 m-1"
+                                                            ></Image>{" "}
+                                                            GITHUB
+                                                        </span>
+                                                    </li>
+
+                                                </ul>
+                                            </Card.Body>
+                                        </Accordion.Collapse>
+                                    </Card>
+                                </Accordion>
+                            </div>
+                            <div className="d-flex justify-content-between flex-nowrap text-center">
+                                <UrlButton
+                                    href="https://getgitusers.netlify.app/"
+                                    target="_blank"
+                                >
+                                    SEE LIVE
+                                </UrlButton>
+                                {/* <UrlButton
+                                    href="https://github.com/Kartik-Mahato/instagram-clone"
+                                    target="_blank"
+                                >
+                                    SOURCE CODE
+                                </UrlButton> */}
+                            </div>
+                        </div>
+                    </ImageEvent>
                     <ImageEvent
                         date="05/04/2021"
                         className="text-center"
@@ -108,6 +197,7 @@ const ProjectTimeline = () => {
                             </div>
                         </div>
                     </ImageEvent>
+                
                 </Events>
             </Timeline>
         </div>
